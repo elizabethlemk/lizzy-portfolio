d6e5a789a4f1659b78a62504831f4094443ce5ac
Elizabeth Le <elizabethlemk@yahoo.com>
2019-04-24 18:20:03 -0400

    form

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Segment } from "semantic-ui-react";
+import { Button, Form, Segment } from "semantic-ui-react";
 
 export default class Contact extends React.Component {
   render() {
-    return <Segment>Contact</Segment>;
+    return (
+      <Segment>
+        <Form>
+          <Form.Input label="Your name" placeholder="First and last name" />
+          <Form.Input label="Email" placeholder="example@email.com" />
+          <Form.Input label="Title" placeholder="In regards to...." />
+          <Form.TextArea
+            label="Content"
+            placeholder="Hi Elizabeth, I wanted to reach out about...."
+          />
+          <Button>Submit</Button>
+        </Form>
+      </Segment>
+    );
   }
 }
