4aca89cf6575f9f001e692ada5be520c985f1838
Elizabeth Le <elizabethlemk@yahoo.com>
2019-04-24 22:28:02 -0400

    contact form works

--- src/Contact.js
+++ src/Contact.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Form, Header, Segment } from "semantic-ui-react";
+import ReactContactForm from "react-mail-form";
 
 export default class Contact extends React.Component {
   render() {
     return (
-      <Segment>
-        <Form>
-          <Form.Input label="Your name" placeholder="First and last name" />
-          <Form.Input label="Email" placeholder="example@email.com" />
-          <Form.Input label="Title" placeholder="In regards to...." />
-          <Form.TextArea
-            label="Content"
-            placeholder="Hi Elizabeth, I wanted to reach out about...."
-          />
-          <Button>Submit</Button>
-        </Form>
+      <Segment textAlign="center">
+        <Header style={{ marginTop: "2rem" }} id="contact-header">
+          To: elizabethlemk@gmail.com
+        </Header>
+        <ReactContactForm
+          to="elizabethlemk@gmail.com"
+          className="contact-form"
+          titlePlaceholder="In regards to...."
+          contentsPlaceholder="Hi Elizabeth, I wanted to reach out about...."
+          buttonText="Submit"
+        />
+        {
+          // <Form>
+          //   <Form.Input label="Your name" placeholder="First and last name" />
+          //   <Form.Input label="Email" placeholder="example@email.com" />
+          //   <Form.Input label="Title" placeholder="In regards to...." />
+          //   <Form.TextArea
+          //     label="Content"
+          //     placeholder="Hi Elizabeth, I wanted to reach out about...."
+          //   />
+          //   <Button>Submit</Button>
+          // </Form>
+        }
       </Segment>
     );
   }
--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Embed, Grid, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Embed,
+  Grid,
+  Icon,
+  Image,
+  Modal,
+  Segment
+} from "semantic-ui-react";
 
 export default class Projects extends React.Component {
   render() {
@@ -8,51 +16,107 @@ export default class Projects extends React.Component {
         <Grid.Column>
           <Segment textAlign="center">
             Project Book
-            <Embed
-              id="dg2nIqLr84M"
-              placeholder="https://camo.githubusercontent.com/1ca7f8140712639a38c1e99f585fbc7207680237/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f78556f434d4d6371566557496841736e72392f67697068792e676966"
-              source="youtube"
-            />
+            <Modal trigger={<Image src="https://tinyurl.com/y6oaoylr" />}>
+              <Modal.Header>Project Book</Modal.Header>
+              <Modal.Content>
+                <Embed
+                  id="dg2nIqLr84M"
+                  placeholder="https://tinyurl.com/y6oaoylr"
+                  source="youtube"
+                />
+                <Modal.Description>Stuff about the project</Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary>
+                  Proceed <Icon name="right chevron" />
+                </Button>
+              </Modal.Actions>
+            </Modal>
           </Segment>
         </Grid.Column>
+
         <Grid.Column>
           <Segment textAlign="center">
-            InnerMost
-            <Embed
-              id="O6Xo21L0ybE"
-              placeholder="https://github.com/csamelian27/Inner-Most/raw/master/public/Screen%20Shot%202019-04-13%20at%208.09.45%20PM.png?raw=true"
-              source="youtube"
-            />
+            The Jelly Club
+            <Modal trigger={<Image src="https://tinyurl.com/y5y4fpzd" />}>
+              <Modal.Header>The Jelly Club</Modal.Header>
+              <Modal.Content>
+                <Embed
+                  id="51LZnX51dgU"
+                  placeholder="https://tinyurl.com/y5y4fpzd"
+                  source="youtube"
+                />
+                <Modal.Description>Stuff about the project</Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary>
+                  Proceed <Icon name="right chevron" />
+                </Button>
+              </Modal.Actions>
+            </Modal>
           </Segment>
         </Grid.Column>
         <Grid.Column>
           <Segment textAlign="center">
             Flatiron Ware Game
-            <Embed
-              id="VQzxto20pow"
-              placeholder="https://camo.githubusercontent.com/82ec25aa0c7fbcfd3f3058760e28f9d309eb34b7/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f696961766e63634e54464c666a59646a386d2f67697068792e676966"
-              source="youtube"
-            />
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment textAlign="center">
-            The Jelly Club
-            <Embed
-              id="51LZnX51dgU"
-              placeholder="https://github.com/elizabethlemk/the-jelly-club/raw/master/app/assets/images/Screen%20Shot%202019-02-15%20at%2012.46.55%20PM.png"
-              source="youtube"
-            />
+            <Modal trigger={<Image src="https://tinyurl.com/y2v7bgvf" />}>
+              <Modal.Header>Flatiron Ware Game</Modal.Header>
+              <Modal.Content>
+                <Embed
+                  id="VQzxto20pow"
+                  placeholder="https://tinyurl.com/y2v7bgvf"
+                  source="youtube"
+                />
+                <Modal.Description>Stuff about the project</Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary>
+                  Proceed <Icon name="right chevron" />
+                </Button>
+              </Modal.Actions>
+            </Modal>
           </Segment>
         </Grid.Column>
         <Grid.Column>
           <Segment textAlign="center">
             The Flatiron Dating Sim
-            <Embed
-              id="tpzmLxTMLSc"
-              placeholder="https://github.com/elizabethlemk/Mod-1-Final-Flatiron-Dating-Sim/raw/master/Screen%20Shot%202019-01-25%20at%209.39.06%20PM.png"
-              source="youtube"
-            />
+            <Modal trigger={<Image src="https://tinyurl.com/y2gqllm5" />}>
+              <Modal.Header>The Flatiron Dating Sim</Modal.Header>
+              <Modal.Content>
+                <Embed
+                  id="tpzmLxTMLSc"
+                  placeholder="https://tinyurl.com/y2gqllm5"
+                  source="youtube"
+                />
+                <Modal.Description>Stuff about the project</Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary>
+                  Proceed <Icon name="right chevron" />
+                </Button>
+              </Modal.Actions>
+            </Modal>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment textAlign="center">
+            InnerMost
+            <Modal trigger={<Image src="https://tinyurl.com/yypeg3j3" />}>
+              <Modal.Header>Inner most</Modal.Header>
+              <Modal.Content>
+                <Embed
+                  id="O6Xo21L0ybE"
+                  placeholder="https://tinyurl.com/yypeg3j3"
+                  source="youtube"
+                />
+                <Modal.Description>Stuff about the project</Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary>
+                  Proceed <Icon name="right chevron" />
+                </Button>
+              </Modal.Actions>
+            </Modal>
           </Segment>
         </Grid.Column>
       </Grid>
