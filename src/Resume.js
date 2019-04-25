d4e7d80c813a6eb6b349e13265f6734e0ce593ea
Elizabeth Le <elizabethlemk@yahoo.com>
2019-04-25 12:58:41 -0400

    working pdf

--- src/Resume.js
+++ src/Resume.js
@@ -1,24 +1,13 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
-import { Document, Page } from "react-pdf";
 import resume from "./images/resume.pdf";
 
-export default class Resume extends React.Component {
-  state = { numPages: null, pageNumber: 1 };
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-  render() {
-    const { pageNumber, numPages } = this.state;
-    return (
-      <Segment id="resume">
-        <Document file={resume} onLoadSuccess={this.onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} width={600} />
-        </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-      </Segment>
-    );
-  }
-}
+const Resume = () => {
+  return (
+    <Segment id="resume">
+      <iframe src={resume} width="100%" height="500px" />
+    </Segment>
+  );
+};
+
+export default Resume;
